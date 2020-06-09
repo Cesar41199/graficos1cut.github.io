5d47bb4fc96eb3510333ae0876fb8c6b2e4c04c7
Cesar41199 <alexglez41199@gmail.com>
2020-06-08 20:37:28 -0500

    page
    Page

---
+++ Graficos Cesar/Graficos 4/1 Pixel fijo/pixel.js
@@ -0,0 +1,18 @@
+function ponerPixel(contexto, x, y, color){
+
+  contexto.fillStyle = color; //configura el color para pintado
+
+  //pintar un punto (debe ser 1x1, para mejorar su visualización es de 3x3)
+  contexto.fillRect( x, y, 3, 3 );
+
+}
+
+function dibujar(){ //Esta función es llamada al cargar el documento html
+
+  var canvas = document.getElementById("lienzo"); //accedemos al lienzo de dibujo
+
+  var contexto = canvas.getContext("2d"); //obtenemos el contexto 2d del lienzo
+
+  ponerPixel(contexto, 100, 120, "#0000FF"); //pintamos el pixel en rojo
+
+} 
---
+++ Graficos Cesar/Graficos 7/Ejercicio 11/cpy_imagen.js
@@ -0,0 +1,70 @@
+var lienzoOrigen;
+var lienzoResultado;
+
+var ctxOrigen;
+var ctxResultado;
+
+var imgOrigen;
+var imgResultado;
+
+
+document.getElementById('cargar').addEventListener('change', leerImagen);
+
+
+function prepararResultados(){ 
+  lienzoResultado = document.getElementById('canvas2');
+  ctxResultado = lienzoResultado.getContext('2d');
+  lienzoResultado.height = lienzoOrigen.height;
+  lienzoResultado.width = lienzoOrigen.width;
+  imgOrigen = ctxOrigen.getImageData(0, 0, lienzoOrigen.width, lienzoOrigen.height); 
+  imgResultado = ctxResultado.createImageData(lienzoOrigen.width, lienzoOrigen.height);
+}
+
+function leerImagen(e){      
+  var archivo = e.target.files[0];
+  if(archivo){        
+    var lector = new FileReader();
+    lector.readAsDataURL(archivo);         
+    lector.onload = function(event){
+      ponerImgEnCanvas(event.target.result);
+    }
+  }
+}
+
+function ponerImgEnCanvas(datosImg){  
+  var img = new Image();
+  img.src = datosImg;
+  img.onload = function(){
+    lienzoOrigen = document.getElementById('canvas');
+    ctxOrigen = lienzoOrigen.getContext('2d');
+    lienzoOrigen.width=img.width;
+    lienzoOrigen.height=img.height;
+    ctxOrigen.drawImage(img, 0, 0);
+    prepararResultados();
+  }
+}
+
+
+function copiar(){   
+  var i;
+  
+  for (i = 0; i < imgOrigen.data.length; i+=4){
+   var prom=(imgOrigen.data[i+0]*0.3+imgOrigen.data[i+1]*0.59+imgOrigen.data[i+2]*0.11);
+   
+      imgResultado.data[i+0] = prom +100;
+      imgResultado.data[i+1] = prom + 50;
+      imgResultado.data[i+2] = prom;
+      imgResultado.data[i+3] = imgOrigen.data[i+3];
+  }
+
+  ctxResultado.putImageData(imgResultado, 0, 0);
+ 
+}
+
+
+
+
+
+
+
+
